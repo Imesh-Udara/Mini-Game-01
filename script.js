0684ac2be449a1ce6b2f11d2ed9aa223da099c85
Imesh <2019cs171@stu.ucsc.lk>
2023-10-16 20:49:13 +0530

    add function

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ runImageNumber = 0;
 runAnim = 0;
 
 
+//idleAnimation
+
 function idleAnimation(){
     idleImageNumber = idleImageNumber + 1;
 
@@ -20,6 +22,8 @@ function idleAnimationsStarts(){
     idleAnim = setInterval(idleAnimation, 150);
 }
 
+//run animation
+
 function runAnimation(){
 
     runImageNumber = runImageNumber + 1;
@@ -34,11 +38,52 @@ function runAnimationStarts(){
     clearInterval(idleAnim);
 }
 
+//jumpAnimation
+jumpImageNumber = 0;
+jumpAnimationNumber = 0;
+girlMarginTop = 647;
+
+function jumpAnimation(){
+    jumpImageNumber = jumpImageNumber + 1;
+
+    if (jumpImageNumber <= 5){
+        girlMarginTop = girlMarginTop - 35;
+        girl.style.marginTop  = girlMarginTop + "px";
+    }
+
+    if (jumpImageNumber >= 6){
+        girlMarginTop = girlMarginTop + 35;
+        girl.style.marginTop  = girlMarginTop + "px";
+    }
+
+
+    if(jumpImageNumber == 10){
+        jumpImageNumber = 0;
+        clearInterval(jumpAnimationNumber);
+        jumpAnimationNumber = 0;
+        runImageNumber = 0;
+        runAnimationStarts();
+    }
+    girl.src = "images/character/Jump__00"+jumpImageNumber+".png ";
+}
+
+function jumpAnimationStarts(){
+
+    clearInterval(idleAnim);
+    runImageNumber = 0;
+    clearInterval(runAnim);
+    jumpAnimationNumber = setInterval(jumpAnimation,100);
+}
+
+
+//key check code
+
 moveBackgroundAnimationId = 0;
 
 function keyCheck(event){
     // alert(event.which);
     // enter = 13
+    // space = 32
 
     var keyCode = event.which;
 
@@ -49,15 +94,102 @@ function keyCheck(event){
         if(moveBackgroundAnimationId == 0){
             moveBackgroundAnimationId = setInterval(moveBackground,100);
         }
+
+        if(boxAnimationId == 0){
+            boxAnimationId = setInterval(boxAnimation,100);
+        }
+    }
+
+    if(keyCode == 32){
+        if(jumpAnimationNumber == 0){
+            jumpAnimationStarts();
+        }
+
+        if(moveBackgroundAnimationId == 0){
+            moveBackgroundAnimationId = setInterval(moveBackground,100);
+        }
+        if(boxAnimationId == 0){
+            boxAnimationId = setInterval(boxAnimation,100);
+        }
     }
 
     
 }
 
 var backgroundPositionX = 0;
+var score = 0;
 
 function moveBackground(){
 
     backgroundPositionX = backgroundPositionX - 20;
     document.getElementById("background").style.backgroundPositionX = backgroundPositionX + "px";
+    score = score + 1;
+    document.getElementById("score").innerHTML = score;
+}
+
+boxMarginLeft = 1540;
+
+function createBoxes(){
+
+    for(var i=0;i<= 10; i++){
+
+    var box = document.createElement("div");
+    box.className = "box";
+    document.getElementById("background").appendChild(box);
+    box.style.marginLeft = boxMarginLeft + "px";
+    box.id = "box" + i ;
+
+    // boxMarginLeft = boxMarginLeft + 800;
+
+    if(i<5){
+        boxMarginLeft = boxMarginLeft + 2000;
+    }
+
+    if(i>=5){
+        boxMarginLeft = boxMarginLeft + 1000;
+    }
+
+    }
+
+    
+}
+
+var boxAnimationId = 0;
+function boxAnimation(){
+    for(var i=0; i<10; i++){
+        var box = document.getElementById("box"+i);
+        var currentMarginLeft = getComputedStyle(box).marginLeft;
+        var newMarginLeft = parseInt(currentMarginLeft) - 35;
+        box.style.marginLeft = newMarginLeft + "px";
+
+        if (newMarginLeft >= -110 & newMarginLeft <= 100){
+            if(girlMarginTop > 600){
+                clearInterval(boxAnimationId);
+        
+                clearInterval(runAnim);
+                runAnim = -1;
+        
+                clearInterval(jumpAnimationNumber);
+                jumpAnimationNumber = -1;
+        
+                clearInterval(moveBackgroundAnimationId);
+                moveBackgroundAnimationId = -1;
+
+                deadAnimationNumber = setInterval(girlDeadAnimation,100);
+        
+            }
+        }
+    }
+}
+
+
+deadImageNumber = 0;
+deadAnimationNumber = 0;
+function girlDeadAnimation(){
+    deadImageNumber = deadImageNumber +1;
+
+    if(deadImageNumber == 10){
+        deadImageNumber = 9
+    }
+    girl.src = "images/character/Dead__00"+deadImageNumber+".png ";
 }
